646d6b6c6a4bb8758983561881152f260ee91757
chong <chong@LAPTOP-E0AVEGV7>
2021-02-02 21:15:45 +0000

    fix bug

--- video-processing/createVid.m
+++ video-processing/createVid.m
@@ -1,8 +1,11 @@
-function vid = createVid(vidname, fps, frameNum)
-% function to create video (.avi) from a folder contraining image frames
-% vidname: output video file name <char>
-% fps: frames per second <double>
-% frameNum: total number of frames <double>
+function vid = createVid(vidname, fps, framesFolder, frameNum)
+% function to create video (.avi) from a folder containing image frames
+%
+%   vidname: output video file name <char>
+%   fps: frames per second <double>
+%   framesFolder: name of folder containing the extracted frames overlaid
+%   with bounding boxes <char>
+%   frameNum: total number of frames <double>
 
 vid = VideoWriter(vidname); %create video object
 vid.FrameRate = fps; % set frame rate
@@ -10,7 +13,7 @@ vid.FrameRate = fps; % set frame rate
 open(vid); %open the video file for writing 
 
 for i = 1:frameNum
-    file_name = sprintf('%d.jpg', i);
+    file_name = fullfile(framesFolder,sprintf('%d.jpg', i));
     I = imread(file_name); %read image 
     writeVideo(vid, I); %write image to file
 end
