b2f1afb78505f5079f69dc650c5afc8b80a89a97
chong <chong@LAPTOP-E0AVEGV7>
2021-02-02 21:14:11 +0000

    Added code for noise object removal and focusing on main flow channel

--- video-processing/vidFramePreprocess.m
+++ video-processing/vidFramePreprocess.m
@@ -1,13 +1,17 @@
-function vidFramePreprocess(filename, bg, t, foldername)
+function vidFramePreprocess(filename, bg, t, minArea, leadEdge, foldername)
 % Function to pre-process individual video frame and draw bounding boxes
-% over identified drops. Output is a lablled image and will be saved in a
+% over identified drops. Output is a lablled image which will be saved in a
 % user-defined directory.
-% Workflow: background subtraction; tresholding & binarisation;
-% morphological close and fill; draw bounding box
+%
+% Workflow: background subtraction -> tresholding & binarisation ->
+% morphological fill -> noise object removal -> draw bounding box
 %
 %   filename: 'video frame file name' <char>
 %   bg: background image file name <var>
 %   t: treshold value (range: 0-1)<double>
+%   minArea: minimum pixel area to keep <double>
+%   leadEdge: x-coordinate of point of entrance to main flow <double> 
+%   foldername: name of folder to save new video frames in 
 
 %read image 
 img = imread(filename); 
@@ -24,10 +28,16 @@ bin_img = imbinarize(sub_img, t);
 fill_img = bwconvhull(bin_img,'objects'); 
 % figure; imshow(fill_img)
 
+%remove noise objects
+cfill_img = bwareaopen(fill_img, minArea);
+
 %detect region of interest 
-roi = regionprops(fill_img,'centroid','BoundingBox'); 
-centres = cat(1,roi.Centroid); 
-box = cat(1,roi.BoundingBox); 
+roi = regionprops('table',cfill_img);
+roi(roi.Centroid(:,1) < leadEdge, :) =[]; %remove data for any points out of main channel
+
+% roi = regionprops(cfill_img,'centroid','BoundingBox'); 
+% centres = cat(1,roi.Centroid); 
+% box = cat(1,roi.BoundingBox); 
 
 %label drops on original video frame and save file 
 f = figure('visible', 'off'); % don't want to display plot when run code
@@ -35,10 +45,10 @@ imshow(filename);
 
 hold on 
 
-plot(centres(:,1), centres(:,2), 'b+'); %draw centroids
+plot(roi.Centroid(:,1), roi.Centroid(:,2), 'b+'); %draw centroids
 
-for i=1:length(box) %draw bounding boxes
-    rectangle('Position',box(i,:),'EdgeColor','b')
+for i=1:height(roi) %draw bounding boxes
+    rectangle('Position',roi.BoundingBox(i,:),'EdgeColor','b')
 end
 
 hold off 
