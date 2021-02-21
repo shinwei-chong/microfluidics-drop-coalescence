69b38e5060bf8147114573b108fb498579220e7a
SWChong <shinwchong@gmail.com>
2021-02-21 14:13:06 +0000

    V2.0

--- video-processing/bboxVis.m
+++ video-processing/bboxVis.m
@@ -1,33 +1,38 @@
 % SUMMARY: Script to process a user-input video and output a new video to
 % visualise the bounding boxes. 
-
+%
 % USER NOTES: 
 % 1)Script file should be stored in the folder along with all the videos 
 %   (.avi format) intended to be processed.
-% 2)Just run script file and it should automatically do the job.
-% 3)Each video processed will output:
-%    - folder containing original extracted frames* 
-%    - folder containing extracted frames with bounding boxes visualised*
+% 2)Each video processed will output:
+%    - folder containing original extracted frames 
 %    - video file with bounding boxes visualised
-% * explicitly exported images as folders for now for debugging/ validation
-% purposes; but if considered unnecessary, can update code to remove this,
-% which should also make processing time much quicker.
-
-% WORKFLOW: 
-% 1) Extract individual frames from video (function:extractVidFrames)
-% 2) Obtain bounding box info on individual drops (function: vidFramePreprocess)
-% 3) Create new video from video frames overlaid with bounding boxes (function: createVid)
-% Note that all functions are at the end of the script
-
+%
+% HIGH-LEVEL WORKFLOW: 
+% 1) Extract individual frames from video 
+% 2) Obtain bounding box info on individual drops 
+% 3) Create new video from video frames overlaid with bounding boxes 
+%
 % DEFAULT PARAMETERS: 
-% 1) background image generation method: median pixel value across 200 frames
+% 1) background image generation method: Complex method - original version,
+%    based on 40 randomly selected frames. 
+%    Other options: Basic statistical approach (median/ mean / mode);
+%                   Complex method - modified version (for C12Tab videos)
+%
 % 2) number of frames extracted from each video: ALL frames
-% 3) treshold value for binarisation: 0.2
-% 4) cropped image dimensions: 128x128 pixels
-% 5) minimum area for region of interest (ROI) detection: 1800 pix^2 
-%    (anything smaller will be treated as noise object
-% 6) leading edge for ROI detection: x==140 (aims to only detect drops in main
-% flow channel)
+%
+% 3) background subtraction method: standard difference + inversion.
+%    Other option: absolute difference (use for C12Tab videos)
+%
+% 4) treshold value for binarisation: determine using Otsu's method
+%    Note: for C12 Tab videos, need to also apply standard filter and
+%    scale-down factor of threshold value determine from Otsu's method.
+%    Change this in segDrop.m function file.
+%
+% 5) leading edge for ROI detection: x=100 (any drops with x-coord <100 will be removed; 
+%    aims to only detect drops in main flow channel).
+%    Note: for vertical flows, change this to 0! Otherwise, all drops would
+%    be removed. 
 
 % Version 2.0. SWC, 19-Feb-2021.
 
@@ -58,7 +63,7 @@ for i = 1: numel(vid_idx) % loop through each video in directory
     
     % ----- using modified statistical approach -----
     n = 40; % number of frames to use for background generation  <-------- user-defined input!!
-    method = 'modified'; % <-------- user-defined input!!
+    method = 'original'; % <-------- user-defined input!!
     % 'original': WAT, DYE, TRI, SDS, 
     % 'modified': C12Tab, TRI, SDS
     bg = bgGenCmplx(n,method); %custom function
@@ -84,41 +89,47 @@ for i = 1: numel(vid_idx) % loop through each video in directory
     
     disp('Frames extraction complete.')
     
-    %% ===== Detect drops in each frame =====
+    %% ===== Detect drops in each frame + Write to new video =====
     disp('Start drop segmentation process...') 
     % allow access to individual frames for processing
     access_folder = dir([frames_folder,'\*.jpg']);
     len_acc_fold = numel(access_folder); 
     
-    % preallocate array to store movie frames
-%     F(len_acc_fold) = struct('cdata',[],'colormap',[]);
+    % => CREATE OBJECT VIDEO
     new_vid_name = ['Bbox_',vid_name]; %new video file name
-    new_vid = VideoWriter(new_vid_name); %create video object
+    new_vid = VideoWriter(new_vid_name); 
     new_vid.FrameRate = vid.FrameRate; %set frame rate
-    
     open(new_vid); %open video file for writing
 
+    % => SET UP PROGRESS BAR
+    pbar = waitbar(0,sprintf('Frame 1 of %d', len_acc_fold),'Name','Creating video');  
     
-    % set up progress bar
-    pbar = waitbar(0,sprintf('Frame 1 of %d', len_acc_fold),'Name','Segmenting drops');  
-    
-    %loop through each frame in subfolder of extracted frames
+    % loop through each frame in subfolder of extracted frames
     for k = 1:len_acc_fold
-        filename = fullfile(frames_folder,sprintf('%d.jpg',k));
+        filename = fullfile(frames_folder,sprintf('%d.jpg',k)); %get file name
         img = imread(filename); %read image
         
-        %background subtraction 
-%         sub_img = rescale(1-(double(img) - double(bg))); %std diff + inversion.
-        sub_img = rescale(abs(double(img) - double(bg))); %abs diff (for C12Tab videos)
-%         figure; imshow(sub_img)
+        % => BACKGROUND SUBTRACTION
+        % ----- standard difference + inversion -----
+        % SUITABLE FOR: WAT, SDS, TRI, DYE
+        sub_img = rescale(1-(double(img) - double(bg)));
         
-        %segment drops
+        % ----- absolute difference -----
+        % USE FOR: C12Tab
+%         sub_img = rescale(abs(double(img) - double(bg))); 
+
+        %figure; imshow(sub_img)
+        
+
+        % => SEGMENT DROPS
         mask = segDrop(sub_img); %custom function
 %         figure; imshow(mask)
 
-        %detect region of interest
+        % => DETECT REGION OF INTEREST
         roi = regionprops('table',mask);
-         % COMMENTED OUT: 20-Feb-2021. May lead to inaccurate estimation in some cases.     
+        
+        
+%% COMMENTED OUT: 20-Feb-2021. May lead to inaccurate estimation in some cases.     
 %         % estimate point of entrance to main channel - only have to do this
 %         % once.
 %         if k == 1
@@ -126,17 +137,17 @@ for i = 1: numel(vid_idx) % loop through each video in directory
 %             % first entry in ROI table is the drop nearest to left frame border
 %             leadEdge = roi.Centroid(1,1) + 1.4*(roi.BoundingBox(1,3) /2); 
 %         end 
+%%
 
-        
-        %remove data for any points out of main channel
-        leadEdge = 0;
+        % remove data for any points out of main channel
+        leadEdge = 100;
         roi(roi.Centroid(:,1) < leadEdge, :) =[]; 
         %remove small objects
         roi(roi.Area < 80, :) = [];
         
-        %draw centroid and bounding box on frame
+        % => DRAW CENTROID + BOUNDING BOX ON FRAME
         f = figure('visible', 'off'); % don't want to display plot when run code
-%         figure; 
+
         imshow(filename); 
         
         hold on 
@@ -150,42 +161,23 @@ for i = 1: numel(vid_idx) % loop through each video in directory
         hold off
         
         F = getframe(gcf); 
-        writeVideo(new_vid, F);
+        writeVideo(new_vid, F); %write the newly generated frame to video
         
-%         drawnow 
         close(f);
         
-        % update progress bar
+        % => UPDATE PROGRESS BAR
         waitbar(k/len_acc_fold,pbar,sprintf('Frame %d of %d', [k len_acc_fold]));
+        
     end
     
-    disp('Drop segmentation process complete');
-    close(pbar)
+    close(pbar); %close progress bar
     
-    %% ===== write frames to create a new movie =====
-%     disp('Creating new movie...')
-    
-%     new_vid_name = ['Bbox_',vid_name]; %new video file name
-%     new_vid = VideoWriter(new_vid_name); %create video object
-%     new_vid.FrameRate = vid.FrameRate; %set frame rate
-    
-%     open(new_vid); %open video file for writing
-    
-    % loop to convert image to frame
-%     for h = 1:length(F)
-%         new_vid_frame = F(h); 
-%         writeVideo(new_vid, new_vid_frame); 
-%     end
-    
-    close(new_vid);
+    close(new_vid); %close video 
 
-    
     toc % stop timer
     
     disp(['Processing for ', vid_name, ' complete.']);
     disp('---------------------------------------');
     
 end
- 
-
 
